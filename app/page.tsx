745bce12a239b8ac2781e36d936803bb9bf86e80
Saad Syed <sn3syed@uwaterloo.ca>
2025-09-15 14:39:51 -0400

    favicon

--- app/page.tsx
+++ app/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
         <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
         <div className="my-16 text-center animate-fade-in">
           <h2 className="text-sm text-zinc-500 ">
-            Passionate about embedded systems, AI, and building technology with real-world impact.
+            Passionate about all things Digital Hardware! Looking to innovate in the field of ML Hardware, FPGA, or ASIC Design.
           </h2>
         </div>
       </div>
